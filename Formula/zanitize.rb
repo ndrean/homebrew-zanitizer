90aab15b18993c4be099c9db119ceeb26c463996
Neven DREAN <nevendrean@yahoo.fr>
2026-03-09 16:10:12 +0100

    v0.20
    Added --allow-attr-prefix

--- Formula/zanitize.rb
+++ Formula/zanitize.rb
@@ -7,18 +7,18 @@ class Zanitize < Formula
   on_macos do
     on_arm do
       url "https://github.com/ndrean/zanitizer/releases/download/v#{version}/zan-darwin-arm64.tar.gz"
-      sha256 "b3e5b42f6017d8e718ba2cb23ae80bcd01fb1928c10f93d06b230a5e6cd88c5d"
+      sha256 "726179e92489df23bdb2501c2930e44f0fd6c609b941fdd08c5798eddf78b720"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/ndrean/zanitizer/releases/download/v#{version}/zan-linux-x86_64.tar.gz"
-      sha256 "cac646896418163498e9e7f6a7f4c854d4b83aee695bfe2c5cdb693c2f963f38"
+      sha256 "917a2fe917480e61a13635ce65987bd370d8ed1631b7d3c9edfabacb9109c650"
     end
     on_arm do
       url "https://github.com/ndrean/zanitizer/releases/download/v#{version}/zan-linux-aarch64.tar.gz"
-      sha256 "6bd9c1f830f69d737b4d5bbe406f05aab452bd7ac2df9fce1437676681bb92fe"
+      sha256 "2a4c888ccf3220aaacf0f6e86b3945dd2fef8f1e516ef82424a2cf706944924c"
     end
   end
   
