21c6d8071ba7b66dc3129647884b10d5a83363dd
Neven DREAN <nevendrean@yahoo.fr>
2026-03-09 08:47:34 +0100

    Update loadFragment -f

--- Formula/zanitize.rb
+++ Formula/zanitize.rb
@@ -1,24 +1,24 @@
 class Zanitize < Formula
   desc "Fast HTML+CSS sanitizer — DOM-aware, not regex-based"
   homepage "https://github.com/ndrean/zanitizer"
-  version "0.1.5"
+  version "0.1.7"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/ndrean/zanitizer/releases/download/v#{version}/zan-darwin-arm64.tar.gz"
-      sha256 "8639d453c4d32a042083ed01dc36ce13d0c8eb7f3c788d7ce51f334a86dcb2c2"
+      sha256 "b3e5b42f6017d8e718ba2cb23ae80bcd01fb1928c10f93d06b230a5e6cd88c5d"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/ndrean/zanitizer/releases/download/v#{version}/zan-linux-x86_64.tar.gz"
-      sha256 "9c44b3c8b632f5a1177a3ae49769ff7e0a06bfa5685509fd49e3700b218de50d"
+      sha256 "cac646896418163498e9e7f6a7f4c854d4b83aee695bfe2c5cdb693c2f963f38"
     end
     on_arm do
       url "https://github.com/ndrean/zanitizer/releases/download/v#{version}/zan-linux-aarch64.tar.gz"
-      sha256 "c5b232abb9ba6640a8a68a8cda8171ca09dd644ab78c216a2a78a398f10c7985"
+      sha256 "6bd9c1f830f69d737b4d5bbe406f05aab452bd7ac2df9fce1437676681bb92fe"
     end
   end
   
